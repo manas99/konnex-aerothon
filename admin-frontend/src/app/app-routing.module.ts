f9b2aeba53a29aea25d0ad2fff0bddb92c0e967a
AmritaNayak1212 <48171914+AmritaNayak1212@users.noreply.github.com>
2021-05-20 23:57:13 +0530

    Hey Manas, happy working!

--- admin-frontend/src/app/app-routing.module.ts
+++ admin-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarNavComponent } from './layouts/sidebar-nav/sidebar-nav.component';
+import { FullPageComponent } from './layouts/full-page/full-page.component';
+import { AnnouncementsComponent } from './views/announcements/announcements.component';
+import { TutorialsComponent } from './views/tutorials/tutorials.component';
+import { BugReportComponent } from './views/bug-report/bug-report.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { UsersComponent } from './views/users/users.component';
+import { LoginComponent } from './views/login/login.component';
+import { ChatComponent } from './views/chat/chat.component';
+import { FeedbackComponent } from './views/feedback/feedback.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: '',
+        component: SidebarNavComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'announcements', component: AnnouncementsComponent },
+            { path: 'tutorials', component: TutorialsComponent },
+            { path: 'bug-report', component: BugReportComponent },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'users', component: UsersComponent },
+            { path: 'chat', component: ChatComponent },
+            { path: 'feedback', component: FeedbackComponent },
+        ],
+        //canActivate: [AuthGuard]
+    },
+    {
+        path: '',
+        component: FullPageComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- admin-frontend/src/app/app.module.ts
+++ admin-frontend/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnnouncementsComponent } from './views/announcements/announcements.component';
+import { TutorialsComponent } from './views/tutorials/tutorials.component';
+import { BugReportComponent } from './views/bug-report/bug-report.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { UsersComponent } from './views/users/users.component';
+import { LoginComponent } from './views/login/login.component';
+import { ChatComponent } from './views/chat/chat.component';
+import { FeedbackComponent } from './views/feedback/feedback.component';
+import { FullPageComponent } from './layouts/full-page/full-page.component';
+import { SidebarNavComponent } from './layouts/sidebar-nav/sidebar-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnouncementsComponent,
+    TutorialsComponent,
+    BugReportComponent,
+    DashboardComponent,
+    UsersComponent,
+    LoginComponent,
+    ChatComponent,
+    FeedbackComponent,
+    FullPageComponent,
+    SidebarNavComponent
   ],
   imports: [
     BrowserModule,
