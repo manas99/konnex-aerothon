c31900fc3cc5bc8b70d98ec59cb2d1ecbac00722
AmritaNayak1212 <48171914+AmritaNayak1212@users.noreply.github.com>
2021-05-21 14:25:52 +0530

    Hey ya Manas!

--- admin-frontend/src/app/views/announcements/announcements.component.ts
+++ admin-frontend/src/app/views/announcements/announcements.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnouncementsComponent implements OnInit {
 
+  records = [{id: 1, description: "ahuiewir  fuuffjhc", date_time: "01/01/2020", is_enabled: true},
+             {id: 2, description: "ahuiewir  fuuffjhc", date_time: "01/01/2020", is_enabled: true},
+             {id: 3, description: "ahuiewir  fuuffjhc", date_time: "01/01/2020", is_enabled: true},
+             {id: 4, description: "ahuiewir  fuuffjhc", date_time: "01/01/2020", is_enabled: true},]
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
