5adaa49ce3e07638637a26f93120eae19709bc56
Manas Oswal <oswal.manu@gmail.com>
2021-05-21 16:53:35 +0530

    Update views.py

--- backend/api/announcements/views.py
+++ backend/api/announcements/views.py
@@ -7,18 +7,23 @@ from .models import Announcement
 from helpers import has_parameters, response_success, response_error
 from django.http import JsonResponse
 
+
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 @has_parameters(['title', 'description', 'client_id'])
 def create(request):
-    Announcement(client_id=request.data['client_id'], title=request.data['title'], description=request.data['description']).save()
+    Announcement(client_id=request.data['client_id'], title=request.data['title'],
+                 description=request.data['description']).save()
     return response_success(message='Announcement created successfully.')
+
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def read(request):
-    announcement = Announcement.objects.all().values('id', 'client_id', 'title', 'description', 'created_at')
-    return JsonResponse(list(announcement), safe=False)
+    announcement = Announcement.objects.all().values(
+        'id', 'client_id', 'title', 'description', 'created_at')
+    return response_success(result=list(announcement))
+
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
