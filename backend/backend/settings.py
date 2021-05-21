9fb8cc8ee45d54324cf6e1013d5c5162d662182f
Manas Oswal <oswal.manu@gmail.com>
2021-05-21 21:05:07 +0530

    dowgraded django and asgiref

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,7 +93,7 @@ ASGI_APPLICATION = "backend.asgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
